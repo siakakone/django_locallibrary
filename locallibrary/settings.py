822c47d51638a01cf3b3e7298b39cc3484721f7e
Siaka Kone <konetcenter@hotmail.com>
2019-11-04 19:12:37 -0500

    setting solved

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 #DEBUG = True
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', 'True'))
 
-ALLOWED_HOSTS = ['serene-sands-04087.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['https://serene-sands-04087.herokuapp.com/','127.0.0.1']
 
 
 # Application definition
